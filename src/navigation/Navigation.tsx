3cc63f361423428f7e9c6be39b5639ea35d6c386
Mxk01 <secretcoderz@yahoo.com>
2023-11-30 17:37:48 +0200

    =Added basic camera  functionality

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -4,6 +4,7 @@ import MultiStep from '../screens/MultiStep'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CarouselMain from '../screens/CarouselMain';
+import FaceRecognition from '../screens/FaceRecognition';
 
 
 // Provides a way for your app to transition between screens where each new screen is placed on top of a stack.
@@ -12,7 +13,8 @@ export default function Navigation () {
     return (
         <NavigationContainer>
           {/*initialRouteName - Sets the default screen of the stack. Must match one of the keys in route configs.*/}
-        <Stack.Navigator initialRouteName='Home'>
+        <Stack.Navigator initialRouteName='FaceDetection'>
+        <Stack.Screen name="FaceDetection" options={{headerShown: false}} component={FaceRecognition} />
         <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
         <Stack.Screen name="MainCarousel" options={{headerShown: false}} component={CarouselMain} />
 
